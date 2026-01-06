686c78c1fca2ffaae1b1d05460cf4a92a76d58bf
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2026-01-06 10:08:11 +0700

    add blur reveal animation

---
+++ app/components/BlurReveal.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup> 
+const props = defineProps({
+  delay: {
+    type: Number,
+    default: 200,
+  },
+  duration: {
+    type: Number,
+    default: 1000,
+  },
+})
+</script>
+
+<template>
+  <div
+    v-motion
+    :initial="{ opacity: 0, filter: 'blur(10px)', y: 20 }"
+    :enter="{
+      opacity: 1,
+      filter: 'blur(0px)',
+      y: 0,
+      transition: {
+        duration: props.duration,
+        type: 'keyframes',
+        ease: 'easeOut',
+        delay: props.delay,
+      },
+    }"
+  >
+    <slot/>
+  </div>
+</template>
