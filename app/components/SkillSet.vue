6e534b280e2cf537787bf1ebd4e648de7e298175
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2026-01-11 22:26:45 +0700

    feat(Home): completed page home

---
+++ app/components/SkillSet.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+const data = [
+  { name: "HTML", icon: "logos:html-5" },
+  { name: "CSS", icon: "logos:css-3" },
+  { name: "Javascript", icon: "logos:javascript" },
+  { name: "PHP", icon: "logos:php" },
+  { name: "Tailwind CSS", icon: "logos:tailwindcss-icon" },
+  { name: "Bootstrap", icon: "logos:bootstrap" },
+  { name: "Laravel", icon: "logos:laravel" },
+  { name: "Node", icon: "logos:nodejs-icon" },
+  { name: "Express", icon: "devicon:express" },
+  { name: "React", icon: "logos:react" },
+  { name: "Tanstack Query", icon: "logos:react-query-icon" },
+  { name: "Next", icon: "logos:nextjs-icon" },
+  { name: "Vue", icon: "logos:vue" },
+  { name: "Nuxt", icon: "logos:nuxt-icon" },
+  { name: "Remix", icon: "logos:remix-icon" },
+  { name: "Sveltekit", icon: "logos:svelte-icon" },
+  { name: "MongoDB", icon: "logos:mongodb-icon" },
+  { name: "Redis", icon: "logos:redis" },
+  { name: "MySQL", icon: "logos:mysql-icon" },
+  { name: "Postgres", icon: "logos:postgresql" },
+  { name: "Supabase", icon: "logos:supabase-icon" },
+  { name: "Prisma", icon: "logos:prisma" },
+  { name: "Postman", icon: "logos:postman-icon" },
+  { name: "Docker", icon: "logos:docker-icon" },
+  { name: "Figma", icon: "logos:figma" },
+  { name: "Git", icon: "logos:git-icon" },
+  { name: "Github", icon: "logos:github-icon" },
+  { name: "Gitlab", icon: "logos:gitlab-icon" },
+  { name: "Vercel", icon: "logos:vercel-icon" },
+  { name: "Netlify", icon: "logos:netlify-icon" },
+  { name: "Arduino", icon: "logos:arduino" },
+  { name: "Obsidian", icon: "logos:obsidian-icon" },
+  { name: "Webstorm", icon: "logos:webstorm" },
+];
+</script>
+<template>
+  <div class="flex flex-row gap-2 items-center flex-wrap">
+    <div
+      v-for="(item, index) in data"
+      :key="index"
+      class="bg-light-contrast-2 dark:bg-dark-contrast-5 rounded-full flex flex-row gap-2 items-center justify-center px-3 py-1.5"
+    >
+      <Icon :name="item.icon" class="size-4" />
+      <p class="text-xs font-medium text-dark-6 dark:text-light-1">
+        {{ item.name }}
+      </p>
+    </div>
+  </div>
+</template>
--- app/pages/home.vue
+++ app/pages/home.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { projects } from "~/data/project";
 definePageMeta({
   layout: "landing-page",
 });
 useSeoMeta({
-  title: "Miftakhussurur Al Maliki | Personal Website",
-  ogTitle: "Miftakhussurur Al Maliki | Personal Website",
-  description: "Halaman utama sistem informasi akademik.",
-  ogDescription: "Halaman utama sistem informasi akademik.",
+  title: "Miftakhussurur Al Maliki - Frontend Developer",
+  ogTitle: "Miftakhussurur Al Maliki - Frontend Developer",
+  description:
+    "Explore the professional portfolio of Miftakhussurur Al Maliki, a Frontend Developer specialized in building modern, responsive, and high-performance web applications.",
+  ogDescription:
+    "Explore the professional portfolio of Miftakhussurur Al Maliki, a Frontend Developer specialized in building modern, responsive, and high-performance web applications.",
 });
 
 const email = "alx.yoi67@gmail.com";
@@ -56,7 +59,7 @@ const handleCopyEmail = async () => {
         <p
           class="text-sm leading-6 text-dark-4 dark:text-dark-contrast-1 text-wrap text-center"
         >
-          Frontend Developer from Malang, ID.<br />
+          Frontend Developer from Malang, Indonesia.<br />
           Currently working at PUSTIK ITN Malang.
         </p>
         <div class="flex flex-row gap-2 mt-4">
@@ -85,22 +88,76 @@ const handleCopyEmail = async () => {
       </div>
     </div>
 
-    <div class="md:flex flex-row gap-2 hidden">
-      <BlurReveal>
-        <h2
-          class="text-dark-contrast-6 dark:text-light-1 font-bold tracking-tighter sm:text-5xl xl:text-4xl/none"
+    <BlurReveal>
+      <div class="md:flex flex-col hidden">
+        <h1
+          class="text-dark-contrast-6 dark:text-light-1 font-bold tracking-tighter text-3xl"
         >
-          I'm
-        </h2>
+          Hey, I'm Mifta!
+        </h1>
+        <div class="flex flex-row items-center gap-2 shrink-0 mt-2">
+          <div class="capitalize flex flex-row items-center gap-2 shrink-0">
+            <div class="rounded-full size-1.5 bg-dark-1 dark:bg-dark-2" />
+            <p
+              class="text-dark-contrast-3 dark:text-light-6 font-medium text-sm leading-6"
+            >
+              Frontend Developer
+            </p>
+          </div>
+          <div class="flex flex-row items-center gap-2 shrink-0">
+            <div class="rounded-full size-1.5 bg-dark-1 dark:bg-dark-2" />
+            <p
+              class="text-dark-contrast-3 dark:text-light-6 font-medium text-sm leading-6"
+            >
+              Based in Malang, East Java, Indonesia
+            </p>
+          </div>
+        </div>
+      </div>
+    </BlurReveal>
+    <div class="flex flex-col gap-8 mt-8 md:mt-4">
+      <BlurReveal :delay="400">
+        <p class="text-base text-dark-5 dark:text-light-3 leading-7">
+          I design and build modern web applications with a strong focus on
+          performance, usability, and clean architecture. Working primarily with
+          the React ecosystem, I help turn ideas into reliable products from
+          early concepts to production-ready solutions. Looking for someone
+          reliable to bring your product to life? Let’s work together.
+        </p>
       </BlurReveal>
-      <BlurReveal :delay="500">
-        <h2
-          class="text-dark-contrast-6 dark:text-light-1 font-bold tracking-tighter sm:text-5xl xl:text-4xl/none"
-        >
-          Miftakhussurur Al Maliki
-        </h2>
+      <BlurReveal :delay="600">
+        <div class="flex flex-col gap-4">
+          <SectionHeader
+            title="Tech Stack"
+            description="The tech stack I use for my personal and client work"
+            icon="solar:code-linear"
+          />
+          <SkillSet />
+        </div>
+      </BlurReveal>
+      <BlurReveal :delay="800">
+        <div class="flex flex-col gap-4">
+          <SectionHeader
+            title="Recent Project"
+            description="A curated showcase of my latest projects and technical work"
+            icon="solar:code-linear"
+          />
+          <div class="grid grid-cols-1 lg:grid-cols-3 gap-4">
+            <MiniProjectItem
+              v-for="(project, index) in projects.slice(0, 3)"
+              :key="index"
+              :project="project"
+            />
+          </div>
+          <NuxtLink
+            to="/project"
+            class="self-center mb-4 flex items-center text-dark-5 dark:text-light-1 justify-center gap-1.5 rounded-full border border-light-5 dark:border-dark-3 w-fit py-2 ps-4 pe-3 text-sm font-medium"
+          >
+            See more
+            <Icon name="solar:arrow-down-linear" />
+          </NuxtLink>
+        </div>
       </BlurReveal>
     </div>
   </Content>
-  <!-- <SkillSet /> -->
 </template>
