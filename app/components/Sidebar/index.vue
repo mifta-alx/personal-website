ba7c70cf1e4c19c3f812004fb75b648084593b35
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2026-05-28 11:33:26 +0700

    ref: change typo about text, and fix layout shrink

--- app/components/Sidebar/index.vue
+++ app/components/Sidebar/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="sticky top-2 md:top-6 z-50 md:h-[calc(100vh-48px)] bg-light-contrast-1 dark:bg-dark-contrast-6 border border-light-2 dark:border-dark-5 rounded-xl w-full md:w-96 py-2 p-2.5 flex flex-col gap-2 justify-between"
+    class="sticky top-2 md:top-6 z-50 md:h-[calc(100vh-48px)] bg-light-contrast-1 dark:bg-dark-contrast-6 border border-light-2 dark:border-dark-5 rounded-xl w-full md:w-72 py-2 p-2.5 flex flex-col shrink-0 gap-2 justify-between" 
   >
     <slot />
   </div>
