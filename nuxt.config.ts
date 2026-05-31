050324e532f1f19bca72b30a45234d71679f8332
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2026-05-31 18:13:54 +0700

    style: update icon meta app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,10 @@ export default defineNuxtConfig({
     head: {
       meta: [
         { name: 'google-site-verification', content: '7AOFh7O3qr2fImEVzTlbw59OI4K-UYmolty8VZ8DW0k' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' }
       ]
     }
   },
@@ -13,12 +17,12 @@ export default defineNuxtConfig({
   css: ['./app/assets/css/main.css'],
   nitro: {
     preset: 'vercel'
-  }, 
+  },
   experimental: {
     payloadExtraction: false
   },
   runtimeConfig: {
-    discordWebhookUrl:process.env.DISCORD_WEBHOOK_URL,
+    discordWebhookUrl: process.env.DISCORD_WEBHOOK_URL,
     public: {
       authorId: process.env.AUTHOR_ID,
     }
