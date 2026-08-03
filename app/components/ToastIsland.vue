3ee597db61c05903b57e7d82dc81ea814f97a92a
Miftakhussurur Al Maliki <alx.yoi67@gmail.com>
2026-08-03 12:54:41 +0700

    style: change toast island color palette

--- app/components/ToastIsland.vue
+++ app/components/ToastIsland.vue
@@ -39,7 +39,7 @@ watch(
       }, 850);
     }
   },
-  { immediate: true }
+  { immediate: true },
 );
 </script>
 
@@ -49,7 +49,7 @@ watch(
     class="fixed top-16 md:top-8 left-0 right-0 flex justify-center z-99 pointer-events-none"
   >
     <div
-      class="flex items-center p-1 overflow-hidden transition-all duration-500 shadow-2xl rounded-full border border-dark-1/10 dark:border-light-1/10 bg-light-1 dark:bg-dark-6 text-dark-6 dark:text-light-1 pointer-events-auto"
+      class="flex items-center p-1 overflow-hidden transition-all duration-500 shadow-2xl rounded-full border border-secondary/10 bg-sidebar text-primary pointer-events-auto"
       :class="[
         isAnchored
           ? 'translate-y-0 opacity-100 scale-100'
@@ -57,12 +57,12 @@ watch(
       ]"
     >
       <div
-        class="flex items-center justify-center rounded-full shrink-0 size-8 text-light-1 shadow-sm"
-        :class="type === 'success' ? 'bg-green-600' : 'bg-red-600'"
+        class="flex items-center justify-center rounded-full shrink-0 size-8 text-white shadow-sm"
+        :class="type === 'success' ? 'bg-emerald-600' : 'bg-red-600'"
       >
         <Icon
-          :name="type === 'success' ? 'iconoir:check' : 'iconoir:xmark'"
-          size="20"
+          :name="type === 'success' ? 'ph:check-bold' : 'ph:x-bold'"
+          class="size-4"
         />
       </div>
 
@@ -72,7 +72,7 @@ watch(
       >
         <div class="overflow-hidden">
           <span
-            class="block px-3 text-sm font-semibold tracking-tight whitespace-nowrap"
+            class="block px-3 text-sm font-medium tracking-tight whitespace-nowrap"
           >
             {{ message }}
           </span>
